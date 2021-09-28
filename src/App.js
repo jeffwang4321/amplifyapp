a37b7de8431512f4f069319ffabfb69d80933f1f
Jeff Wang <jeffwang4321@gmail.com>
2021-09-28 14:03:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V5</h1>
+        <h1>Hello from V6</h1>
       </header>
     </div>
   );
